62dae3878404b0c2cdb1f91d3ca09ba715a8b964
段雨佳 <duanyujia@duanyujias-MacBook-Pro.local>
2018-06-20 14:07:21 +0800

    update decision tree

--- decision_tree/tree.py
+++ decision_tree/tree.py
@@ -1,4 +1,5 @@
 from math import log
+import operator
 
 
 def calcShannonEnt(dataSet):
@@ -26,3 +27,44 @@ def createDataSet():
     ]
     labels = ['no surfacing', 'flippers']
     return dataSet, labels
+
+
+def splitDataSet(dataSet, axis, value):
+    retDataSet = []
+    for featVec in dataSet:
+        if featVec[axis] == value:
+            reducedFeatVec = featVec[:axis]
+            reducedFeatVec.extend(featVec[axis+1:])
+            retDataSet.append(reducedFeatVec)
+    return retDataSet
+
+
+def chooseBestFeatureToSplit(dataSet):
+    numFeatures = len(dataSet[0]) - 1
+    baseEntropy = calcShannonEnt(dataSet)
+    bestInfoGain = 0.0
+    bestFeature = -1
+    for i in range(numFeatures):
+        featList = [example[i] for example in dataSet]
+        uniqueVals = set(featList)
+        newEntropy = 0.0
+        for value in uniqueVals:
+            subDataSet = splitDataSet(dataSet, i, value)
+            prob = len(subDataSet)/float(len(dataSet))
+            newEntropy += prob * calcShannonEnt(subDataSet)
+        infoGain = baseEntropy - newEntropy
+        if (infoGain > bestInfoGain):
+            bestInfoGain = infoGain
+            bestFeature = i
+    return bestFeature
+
+
+def majorityCnt(classList):
+    classCount = {}
+    for vote in classList:
+        if vote not in classCount.keys():
+            classCount[vote] = 0
+        classCount[vote] += 1
+    sortedClassCount = sorted(classCount.iteritems(), \
+                              key=operator.itemgetter(1), reverse=True)
+    return sortedClassCount[0][0]
