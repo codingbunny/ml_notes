a816349218ae7f25d4d357b58b6d0fbf583f1086
段雨佳 <duanyujia@duanyujias-MacBook-Pro.local>
2018-06-22 19:40:34 +0800

    update decision tree

--- decision_tree/treePlotter.py
+++ decision_tree/treePlotter.py
@@ -21,3 +21,28 @@ def plotNode(nodeTxt, centerPt, parentPt, nodeType):
                             va='center', ha='center', bbox=nodeType, arrowprops=arrow_args)
 
 
+def getNumLeafs(myTree):
+    numLeafs = 0
+    firstStr = myTree.keys()[0]
+    secondDict = myTree[firstStr]
+    for key in secondDict.keys():
+        if type(secondDict[key]).__name__ == 'dict':
+            thisDepth = 1  + getTreeDepth(secondDict[key])
+        else:
+            numLeafs += 1
+    return numLeafs
+
+
+def getTreeDepth(myTree):
+    maxDepth = 0
+    firstStr = myTree.keys()[0]
+    secondDict = myTree[firstStr]
+    for key in secondDict.keys():
+        if type(secondDict[key]).__name__ == 'dict':
+            thisDepth = 1 + getTreeDepth(secondDict[key])
+        else:
+            thisDepth = 1
+        if thisDepth > maxDepth:
+            maxDepth = thisDepth
+    return maxDepth
+
