781f71d7626ae653052d054b68bf4c1cdc092655
段雨佳 <duanyujia@duanyujias-MacBook-Pro.local>
2018-06-26 19:29:29 +0800

    update

--- decision_tree/treePlotter.py
+++ decision_tree/treePlotter.py
@@ -5,29 +5,13 @@ leafNode = dict(boxstyle="round4", fc="0.8")
 arrow_args = dict(arrowstyle="<-")
 
 
-def createPlot():
-    fig = plt.figure(1, facecolor='white')
-    fig.clf()
-    createPlot.ax1 = plt.subplot(111, frameon=False)
-    plotNode('decision node', (0.5, 0.1), (0.1, 0.5), decisionNode)
-    plotNode('leaf node', (0.8, 0.1), (0.3, 0.8), leafNode)
-    plt.show()
-
-
-def plotNode(nodeTxt, centerPt, parentPt, nodeType):
-    createPlot.ax1.annotate(nodeTxt, xy=parentPt, \
-                            xycoords='axes fraction', \
-                            xytext = centerPt, textcoords='axes fraction', \
-                            va='center', ha='center', bbox=nodeType, arrowprops=arrow_args)
-
-
 def getNumLeafs(myTree):
     numLeafs = 0
-    firstStr = myTree.keys()[0]
+    firstStr = list(myTree.keys())[0]
     secondDict = myTree[firstStr]
     for key in secondDict.keys():
         if type(secondDict[key]).__name__ == 'dict':
-            thisDepth = 1  + getTreeDepth(secondDict[key])
+            numLeafs += 1 + getTreeDepth(secondDict[key])
         else:
             numLeafs += 1
     return numLeafs
@@ -35,7 +19,7 @@ def getNumLeafs(myTree):
 
 def getTreeDepth(myTree):
     maxDepth = 0
-    firstStr = myTree.keys()[0]
+    firstStr = list(myTree.keys())[0]
     secondDict = myTree[firstStr]
     for key in secondDict.keys():
         if type(secondDict[key]).__name__ == 'dict':
@@ -46,3 +30,58 @@ def getTreeDepth(myTree):
             maxDepth = thisDepth
     return maxDepth
 
+
+def retrieveTree(i):
+    listOfTrees = [{'no surfacing': {0: 'no', 1: {'flippers':\
+                                                      {0: 'no', 1: 'yes'}}}},
+                   {'no surfacing': {0: 'no', 1: {'flippers':\
+                                                      {0: {'head': {0: 'no', 1: 'yes'}, 1: 'no'}}}}}
+                   ]
+    return listOfTrees[i]
+
+
+def plotMideText(cntrPt, parentPt, txtString):
+    xMid = (parentPt[0] - cntrPt[0])/2.0 + cntrPt[0]
+    yMid = (parentPt[1] - cntrPt[1])/2.0 + cntrPt[1]
+    createPlot.ax1.text(xMid, yMid, txtString)
+
+
+def plotTree(myTree, parentPt, nodeTxt):
+    numLeafs = getNumLeafs(myTree)
+    depth = getTreeDepth(myTree)
+    firstStr = list(myTree.keys())[0]
+    cntrPt = (plotTree.xOff + (1.0 + float(numLeafs))/2.0/plotTree.totalW, \
+              plotTree.yOff)
+    plotMideText(cntrPt, parentPt, nodeTxt)
+    plotNode(firstStr, cntrPt, parentPt, decisionNode)
+    secondDict = myTree[firstStr]
+    plotTree.yOff = plotTree.yOff - 1.0/plotTree.totalD
+    for key in secondDict.keys():
+        if type(secondDict).__name__ == 'dict':
+            plotTree(secondDict[key], cntrPt, str(key))
+        else:
+            plotTree.xOff = plotTree.xOff + 1.0/plotTree.totalW
+            plotNode(secondDict[key], (plotTree.xOff, plotTree.yOff), \
+                     cntrPt, leafNode)
+            plotMideText((plotTree.xOff, plotTree.yOff), cntrPt, str(key))
+    plotTree.yOff = plotTree.yOff + 1.0/plotTree.totalD
+
+
+def createPlot(inTree):
+    fig = plt.figure(1, facecolor='white')
+    fig.clf()
+    axprops = dict(xticks=[], yticks=[])
+    createPlot.ax1 = plt.subplot(111, frameon=False, **axprops)
+    plotTree.totalW = float(getNumLeafs(inTree))
+    plotTree.totalD = float(getTreeDepth(inTree))
+    plotTree.xOff = -0.5/plotTree.totalW
+    plotTree.yOff = 1.0
+    plotTree(inTree, (0.5, 1.0), '')
+    plt.show()
+
+
+def plotNode(nodeTxt, centerPt, parentPt, nodeType):
+    createPlot.ax1.annotate(nodeTxt, xy=parentPt, \
+                            xycoords='axes fraction', \
+                            xytext=centerPt, textcoords='axes fraction', \
+                            va='center', ha='center', bbox=nodeType, arrowprops=arrow_args)
