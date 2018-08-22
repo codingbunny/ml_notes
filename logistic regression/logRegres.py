0d8d4fb99a73c9339fbbd07629b8580a86d15ab8
段雨佳 <duanyujia@duanyujias-MacBook-Pro.local>
2018-08-22 19:31:09 +0800

    update svm

--- logistic regression/logRegres.py
+++ logistic regression/logRegres.py
@@ -9,6 +9,7 @@ def loadDataSet():
     fr = open('testSet.txt')
     for line in fr.readlines():
         lineArr = line.strip().split()
+        # z = w0 + w1x1 + w2x2
         dataMat.append([1.0, float(lineArr[0]), float(lineArr[1])])
         labelMat.append(int(lineArr[2]))
     return dataMat, labelMat
@@ -18,6 +19,7 @@ def sigmoid(inX):
     return 1.0 / (1 + np.exp(-inX))
 
 
+# 梯度上升
 def gradAscent(dataMatIn, classLabels):
     dataMatrix = np.mat(dataMatIn)
     labelMat = np.mat(classLabels).transpose()
@@ -55,5 +57,79 @@ def plotBestFit(wei):
     x = np.arange(-3.0, 3.0, 0.1)
     y = (-weights[0] - weights[1] * x) / weights[2]
     ax.plot(x, y)
-    plt.xlabel('X1'); plt.ylabel('X2')
+    plt.xlabel('X1')
+    plt.ylabel('X2')
     plt.show()
+
+
+# 随机梯度
+def stocGradAscent0(dataMatrix, classLabels):
+    m, n = np.shape(dataMatrix)
+    alpha = 0.01
+    weights = np.ones(n)
+    for i in range(m):
+        h = sigmoid(sum(dataMatrix[i] * weights))
+        error = classLabels[i] - h
+        weights = weights + alpha * error * dataMatrix[i]
+    return weights
+
+# 改良随机梯度
+def stocGradAscent1(dataMatrix, classLabels, numIter=150):
+    m, n = np.shape(dataMatrix)
+    weights = np.ones(n)
+    for j in range(numIter):
+        dataIndex = range(m)
+        for i in range(m):
+            # alpha每次迭代都要调整
+            alpha = 4 / (1.0 + j + i) + 0.01
+            randIndex = int(np.random.uniform(0, len(dataIndex)))
+            h = sigmoid(sum(dataMatrix[randIndex] * weights))
+            error = classLabels[randIndex] - h
+            weights = weights + alpha * error * dataMatrix[randIndex]
+            del (dataIndex[randIndex])
+    return weights
+
+
+def classifyVector(inX, weights):
+    prob = sigmoid(sum(inX*weights))
+    if prob > 0/5:
+        return 1.0
+    else:
+        return 0.0
+
+
+def colicTest():
+    frTrain = open('horseColicTraining.txt')
+    frTest = open('horseColicTest.txt')
+    trainingSet = []
+    trainingLabels = []
+    for line in frTrain.readlines():
+        currLine = line.strip().split('\t')
+        lineArr = []
+        for i in range(21):
+            lineArr.append(float(currLine[i]))
+        trainingSet.append(lineArr)
+        trainingLabels.append(float(currLine[21]))
+    trainWeights = stocGradAscent1(np.array())
+    errorCount = 0
+    numTestVec = 0.0
+    for line in frTest.readlines():
+        numTestVec += 1.0
+        currLine = line.strip().split('\t')
+        lineArr = []
+        for i in range(21):
+            lineArr.append(float(currLine[i]))
+        if int(classifyVector(np.array(lineArr), trainWeights) != int(currLine[21])):
+            errorCount += 1
+        errorRate = (float(errorCount) / numTestVec)
+        print("the error rate of this test is: %f" % errorRate)
+        return errorRate
+
+
+def multiTest():
+    numTests = 10
+    errorSum = 0.0
+    for k in range(numTests):
+        errorSum += colicTest()
+    print("after %f iterations the average error rate is:"
+          "%f" % (numTests, errorSum/float(numTests)))
